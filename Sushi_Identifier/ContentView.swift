b504daf1c66b748011b47ff5e5ca2bde68254c91
Fukami <fukaryu421@asukalab.co.jp>
2019-10-31 16:01:37 +0900

    Initial Commit

---
+++ Sushi_Identifier/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sushi_Identifier
+//
+//  Created by 深見龍一 on 2019/10/31.
+//  Copyright © 2019 Asukalab. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sushi_IdentifierTests/Sushi_IdentifierTests.swift
@@ -0,0 +1,34 @@
+//
+//  Sushi_IdentifierTests.swift
+//  Sushi_IdentifierTests
+//
+//  Created by 深見龍一 on 2019/10/31.
+//  Copyright © 2019 Asukalab. All rights reserved.
+//
+
+import XCTest
+@testable import Sushi_Identifier
+
+class Sushi_IdentifierTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
