e52e897e023005fbe9183626fd7a92e20476c597
Fukami <fukaryu421@asukalab.co.jp>
2019-10-31 16:56:03 +0900

    fix searchbar

--- Sushi_Identifier/Views/SearchViewController.swift
+++ Sushi_Identifier/Views/SearchViewController.swift
@@ -8,10 +8,33 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
-
-    override func viewDidLoad() {
-      super.viewDidLoad()
-      self.title = "Search"
-    }
+class SearchViewController: UIViewController, UISearchBarDelegate {
+  @IBOutlet weak var searchBar: UISearchBar!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.title = "Search"
+    self.searchBarSetUp()
+  }
+  
+  func searchBarSetUp()
+  {
+    self.searchBar.delegate = self
+    searchBar.showsCancelButton = false
+    //プレースホルダの指定
+    searchBar.placeholder = "検索文字列を入力してください"
+    //検索スコープを指定するボタン
+    searchBar.scopeButtonTitles  = ["寿司屋", "寿司ネタ"]
+    searchBar.showsScopeBar = true
+  }
+  
+  func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+    searchBar.showsCancelButton = true
+    return true
+  }
+  
+  func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+    searchBar.showsCancelButton = false
+    searchBar.resignFirstResponder()
+  }
 }
